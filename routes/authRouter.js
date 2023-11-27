e8061750885b396f4af4db454b0767af618f0d5d
clarkjoseph74 <77942463+clarkjoseph74@users.noreply.github.com>
2023-11-27 09:42:59 +0200

    Update authRouter.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const  registerController  = require('../controllers/authController');
-const  {loginController}  = require('../controllers/authController');
+
+const  {registerController , loginController}  = require('../controllers/authController');
 const router = express.Router();
 
 
